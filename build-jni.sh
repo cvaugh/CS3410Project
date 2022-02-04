4795dd996b68e4671f44b8e23fdc9fca23db58b0
Chase Vaughan <git@chase-vaughan.com>
2022-02-03 19:47:24 -0500

    Rename C++ file

--- build-jni.sh
+++ build-jni.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -c -I"${JAVA_HOME%}\include" -I"${JAVA_HOME}\include\win32" cs3410_project_filesystem_Main.cpp -o cs3410_project_filesystem_Main.o
-gcc -shared -o native.dll cs3410_project_filesystem_Main.o -Wall
+gcc -c -I"${JAVA_HOME%}\include" -I"${JAVA_HOME}\include\win32" native.cpp -o native.o
+gcc -shared -o native.dll native.o -Wall
